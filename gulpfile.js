24919a3c5a12df02d43c7a67b5535097c66bba2d
Sergi Almar <sergi.almar@gmail.com>
2019-06-17 17:56:23 +0200

    Render templates with Mustache

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 const gulp = require('gulp');
 const mjml = require('gulp-mjml');
+const mjmlEngine = require('mjml')
+const rename = require('gulp-rename');
 
 const srcMjml = './src/main/resources/mjml/**/*.mjml';
-const dstMjml = './src/main/resources/static/';
+const dstMjml = './src/main/resources/templates/';
 
 
 gulp.task('mjmlToHtml', function () {
     return gulp.src([srcMjml])
-        .pipe(mjml())
+        .pipe(mjml(mjmlEngine, {beautify: true}))
+        .pipe(rename(function(path) {
+        	path.extname = '.ms';
+        }))
         .pipe(gulp.dest(dstMjml))
 });
 
--- src/main/java/com/sergialmar/mjml/SpringMjmlGradleApplication.java
+++ src/main/java/com/sergialmar/mjml/SpringMjmlGradleApplication.java
@@ -1,12 +1,26 @@
 package com.sergialmar.mjml;
 
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@Controller
 public class SpringMjmlGradleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMjmlGradleApplication.class, args);
+	}
+	
+	@GetMapping("/welcome")
+	public String renderTemplate(@RequestParam("name") Optional<String> name, Model model) {
+		model.addAttribute("name", name.orElse("Anonymous"));
+		
+		return "welcome";
 	}
 }
